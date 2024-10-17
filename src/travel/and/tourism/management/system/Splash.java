fa1596b399784d8eb9893725d9fe142ce3725a4d
MAHAMUDUL HASAN MITHUN <mhmmithun1@gmail.com>
2024-10-17 12:54:37 +0600

    JAVA PROJECT

--- src/travel/and/tourism/management/system/Splash.java
+++ src/travel/and/tourism/management/system/Splash.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class Splash extends JFrame {
     Splash(){
     setSize(1200,600);
+    setLocation(200,100);
     setVisible(true);
     }
      public static void main(String[] args){
