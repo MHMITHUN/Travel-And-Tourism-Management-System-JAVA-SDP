d9039c999404429a98808b235942dad865b09c97
MAHAMUDUL HASAN MITHUN <mhmmithun1@gmail.com>
2024-11-05 03:15:32 +0600

    JAVA PROJECT

--- src/travel/and/tourism/management/system/Splash.java
+++ src/travel/and/tourism/management/system/Splash.java
@@ -1,14 +1,69 @@
 package travel.and.tourism.management.system;
 
 import javax.swing.*;
-        
-public class Splash extends JFrame {
-    Splash(){
-    setSize(1200,600);
-    setLocation(200,100);
-    setVisible(true);
+import java.awt.*;
+
+public class Splash extends JFrame implements Runnable {
+    Thread thread;
+
+    Splash() {
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/splashscreen.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1200, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+
+        JLabel image = new JLabel(i3);
+        add(image);
+
+        setUndecorated(true);
+        setVisible(true);
+
+        thread = new Thread(this);
+        thread.start();
     }
-     public static void main(String[] args){
-         new Splash();
-     }
+
+    public void run() {
+        try {
+            Thread.sleep(6000);
+            setVisible(false);
+            dispose();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Splash frame = new Splash();
+        
+        int maxWidth = 1200;
+        int maxHeight = 600;
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+
+        int x = 7;
+
+        for (int i = 2; i <= maxHeight; i += 10, x += 7) {
+            int currentWidth = i + x;
+            int currentHeight = i;
+
+            if (currentWidth >= maxWidth && currentHeight >= maxHeight) {
+                break;
+            }
+
+            int xPos = (screenWidth - currentWidth) / 2;
+            int yPos = (screenHeight - currentHeight) / 2;
+            frame.setLocation(xPos, yPos);
+            frame.setSize(currentWidth, currentHeight);
+
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        frame.setLocation((screenWidth - maxWidth) / 2, (screenHeight - maxHeight) / 2);
+        frame.setSize(maxWidth, maxHeight);
+    }
 }
